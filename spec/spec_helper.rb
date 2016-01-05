0fe84f98f193e405381c98c9754a94208a5b496d
Phil Friderici <phil.friderici@ericsson.com>
2016-01-05 15:57:16 +0100

    Support Puppet 4.3, modernize Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,1 @@
-require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
-
-RSpec.configure do |config|
-  config.before :each do
-    Puppet[:parser] = 'future' if ENV['PARSER'] == 'future'
-  end
-end
